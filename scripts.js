bb646b2dc61950efd0d681f15e38de60814ab4a5
Ingrid Bjarman <ingridbjarman@Makerss-MacBook-Pro-109.local>
2020-03-25 22:02:43 +0000

    Scripts modified to accomodate API images, Styling done. HTML for each card now built on function call

--- scripts.js
+++ scripts.js
@@ -1,5 +1,4 @@
-const cards = document.querySelectorAll('.memory-card');
-
+var cards;
 let hasFlippedCard = false;
 let lockBoard = false;
 let firstCard, secondCard;
@@ -25,7 +24,7 @@ function flipCard() {
 }
 
 function checkForMatch() {
-  let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
+  let isMatch = firstCard.dataset.id === secondCard.dataset.id;
 
   isMatch ? disableCards() : unflipCards();
 }
@@ -53,11 +52,46 @@ function resetBoard() {
   [firstCard, secondCard] = [null, null];
 }
 
-(function shuffle() {
+function getCatImagesFromApi() {
+  fetch('https://api.thecatapi.com/v1/images/search?limit=6', {
+  method: "GET",
+  headers: {
+    'x-api-key': 'fd56e6ce-e1a8-4243-92dc-eb6ae6e76f5a' 
+  }})
+  .then(response => {
+    return response.json()
+  })
+  .then(data => {
+    console.log(data)
+    data.forEach((cat, index) => {
+      buildHTML(cat);
+      buildHTML(cat);
+    });
+    cards = document.querySelectorAll('.memory-card');
+
+    cards.forEach(card => card.addEventListener('click', flipCard));
+
+    shuffle();
+  })
+  .catch(err => {
+    throw "Something went wrong! Such a pity, it usually runs purr-fectly..."
+  })
+}
+getCatImagesFromApi();
+
+function buildHTML(cat)
+{
+  let block = `<div class="memory-card" data-id="${cat['id']}">
+    <img class="front-face" src="${cat['url']}" alt="${cat['id']}" />
+    <img class="back-face" src="img/cat.png" alt="cat" />
+  </div>`
+
+  document.getElementById("memory-game").innerHTML += block;
+}
+
+function shuffle() {
   cards.forEach(card => {
     let randomPos = Math.floor(Math.random() * 12);
     card.style.order = randomPos;
   });
-})();
-
-cards.forEach(card => card.addEventListener('click', flipCard));
+};
